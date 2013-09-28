23dbb98a55d6592f11595a473f5c1ba3d8a6ec73
Shawn Mclean <shawnrmclean@gmail.com>
2013-09-27 19:57:07 -0500

    added core project.

---
+++ src/MvvmCross.Plugins.Settings/IMvxSettings.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IMvxSettings.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The MvxSettings interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MvvmCross.Plugins.Settings
+{
+    using System;
+
+    /// <summary>
+    /// The IMvxSettings interface.
+    /// </summary>
+    public interface IMvxSettings
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Adds or updates the value
+        /// </summary>
+        /// <param name="key">
+        /// Key for settting
+        /// </param>
+        /// <param name="value">
+        /// Value to set
+        /// </param>
+        /// <returns>
+        /// True of was added or updated and you need to save it.
+        /// </returns>
+        bool AddOrUpdateValue(string key, object value);
+
+        /// <summary>
+        /// Gets the current value or the default that you specify.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Vaue of t (bool, int, float, long, string)
+        /// </typeparam>
+        /// <param name="key">
+        /// Key for settings
+        /// </param>
+        /// <param name="defaultValue">
+        /// default value if not set
+        /// </param>
+        /// <returns>
+        /// Value or default
+        /// </returns>
+        T GetValueOrDefault<T>(string key, T defaultValue) where T : IComparable;
+
+        /// <summary>
+        ///     Saves the changes.
+        /// </summary>
+        void Save();
+
+        #endregion
+    }
+}
---
+++ src/MvvmCross.Plugins.Settings/PluginLoader.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PluginLoader.cs" company="Shawn McLean">
+//   
+// </copyright>
+// <summary>
+//   Defines the PluginLoader type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MvvmCross.Plugins.Settings
+{
+    using Cirrious.CrossCore;
+    using Cirrious.CrossCore.Plugins;
+
+    /// <summary>
+    /// The plugin loader.
+    /// </summary>
+    public class PluginLoader
+        : IMvxPluginLoader
+    {
+        /// <summary>
+        /// The instance.
+        /// </summary>
+        public static readonly PluginLoader Instance = new PluginLoader();
+
+        /// <summary>
+        /// The ensure loaded.
+        /// </summary>
+        public void EnsureLoaded()
+        {
+            var manager = Mvx.Resolve<IMvxPluginManager>();
+            manager.EnsurePlatformAdaptionLoaded<PluginLoader>();
+        }
+    }
+}

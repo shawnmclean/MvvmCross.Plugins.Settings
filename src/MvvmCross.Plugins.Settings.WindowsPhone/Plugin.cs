c00d6934f00779dbe2db6fe33aaded6a96bf5716
Shawn Mclean <shawnrmclean@gmail.com>
2013-09-28 11:42:01 -0500

    added mvx package.

---
+++ src/MvvmCross.Plugins.Settings.WindowsPhone/Plugin.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Plugin.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Plugin type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MvvmCross.Plugins.Settings.WindowsPhone
+{
+    using Cirrious.CrossCore;
+    using Cirrious.CrossCore.Plugins;
+
+    /// <summary>
+    /// The plugin.
+    /// </summary>
+    public class Plugin : IMvxPlugin
+    {
+        /// <summary>
+        /// The load.
+        /// </summary>
+        public void Load()
+        {
+            Mvx.RegisterSingleton<IMvxSettings>(new MvxSettings());
+        }
+    }
+}
